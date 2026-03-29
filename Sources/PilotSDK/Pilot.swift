d9d10a414ab1be3a07a7a8564feb094f69f4e00d
yurii.levchenko <yurii.levchenko@murka.com>
2026-03-30 00:46:09 +0300

    Fix CI: add CDN trunk repo explicitly (v1.0.39)

--- Sources/PilotSDK/Pilot.swift
+++ Sources/PilotSDK/Pilot.swift
@@ -4,7 +4,7 @@ import UIKit
 #endif
 
 public final class Pilot {
-    public static let version = "1.0.38"
+    public static let version = "1.0.39"
 
     private static var instance: Pilot?
     private static let instanceLock = NSLock()
