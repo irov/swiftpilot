1ab124df954c82c4de79eba345320e31cd66eae8
yurii.levchenko <yurii.levchenko@murka.com>
2026-03-30 04:54:44 +0300

    Fix CocoaPods publish workflow (v1.0.45)

--- Sources/PilotSDK/Pilot.swift
+++ Sources/PilotSDK/Pilot.swift
@@ -4,7 +4,7 @@ import UIKit
 #endif
 
 public final class Pilot {
-    public static let version = "1.0.44"
+    public static let version = "1.0.45"
 
     private static var instance: Pilot?
     private static let instanceLock = NSLock()
